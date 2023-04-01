5ffb2b703f8db68231366d0a3d33e1f1176ac564
lich <lich123good@gmail.com>
2023-04-01 11:38:43 +0800

    字符串1到4

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,23 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut result = String::from(input.trim());
+    return result;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let result = String::from(input.to_owned() + " world!");
+    return result;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+	let result = input.replace("cars", "balloons");
+    return result;
 }
 
 #[cfg(test)]
